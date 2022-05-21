03bf10c827fba5dea2ef2d0cd4100dec829b6560
aflinsjr@icloud.com <aflinsjr@icloud.com>
2022-05-21 16:31:07 -0300

    API w/ rabbit

--- src/rabbitmq-server copy.ts
+++ src/rabbitmq-server copy.ts
@@ -2,10 +2,10 @@ import client, { Connection, Channel, ConsumeMessage } from 'amqplib';
 
 //queue.js
 function connect(){
-  return require('amqplib').connect("amqp://username:password@localhost:5672").then(conn => conn.createChannel());
+  return require('amqplib').connect("amqp://username:password@localhost:5672").then((conn: { createChannel: () => any; }) => conn.createChannel());
 }
  
-function createQueue(channel: Channel, queue){
+function createQueue(channel: Channel, queue: string){
   return new Promise((resolve, reject) => {
     try{
       channel.prefetch(1);
@@ -16,18 +16,18 @@ function createQueue(channel: Channel, queue){
   });
 }
  
-export function sendToQueue(queue, message){
+export function sendToQueue(queue: string, message: any){
   connect()
-    .then(channel => createQueue(channel, queue))
-    .then(channel => channel.sendToQueue(queue, Buffer.from(JSON.stringify(message))))
-    .catch(err => console.log(err))
+    .then((channel: client.Channel) => createQueue(channel, queue))
+    .then((channel: { sendToQueue: (arg0: any, arg1: Buffer) => any; }) => channel.sendToQueue(queue, Buffer.from(JSON.stringify(message))))
+    .catch((err: any) => console.log(err))
 }
  
-export function consume(queue, callback){
+export function consume(queue: string, callback: (message: { content: string; }) => Promise<void>){
   connect()
-    .then(channel => createQueue(channel, queue))
-    .then(channel => channel.consume(queue, callback, { noAck: true }))
-    .catch(err => console.log(err));
+    .then((channel: client.Channel) => createQueue(channel, queue))
+    .then((channel: { consume: (arg0: any, arg1: any, arg2: { noAck: boolean; }) => any; }) => channel.consume(queue, callback, { noAck: true }))
+    .catch((err: any) => console.log(err));
 }
  
 module.exports = {
